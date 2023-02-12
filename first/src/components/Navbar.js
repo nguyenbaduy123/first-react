ab00175581778ff0337aa6cedad0b33debce36ac
nguyenbaduy123 <baduy.hust@gmail.com>
2023-02-12 20:28:49 +0700

    update the first project

--- first/src/components/Navbar.js
+++ first/src/components/Navbar.js
@@ -1,12 +1,19 @@
 import logo from "../images/react-logo.png";
 
-function Navbar() {
+function Navbar(props) {
   return (
     <header>
-      <nav>
+      <nav className={props.darkMode ? "dark" : ""}>
         <img src={logo} className="nav--icon"></img>
         <h3 className="nav--logo__text">ReactFacts</h3>
-        <h4 className="nav--title">React - Project 1</h4>
+
+        <div className="toggler">
+          <p className="toggler--light">Light</p>
+          <div className="toggler--slider" onClick={props.toggleDarkMode}>
+            <div className="toggler--slider--circle"></div>
+          </div>
+          <p className="toggler--dark">Dark</p>
+        </div>
       </nav>
     </header>
   );
