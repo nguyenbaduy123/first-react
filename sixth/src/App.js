24f8e1222f3c0c46aa1095567c94041d66b00896
nguyenbaduy123 <baduy.hust@gmail.com>
2023-02-15 10:30:52 +0700

    done the sixth project

---
+++ sixth/src/App.js
@@ -0,0 +1,86 @@
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import Split from "react-split";
+
+import data from "./data";
+
+function App() {
+  const [notes, setNotes] = useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "something",
+    };
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+    setNotes((oldNotes) => {
+      const newArray = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text });
+        } else {
+          newArray.push(oldNote);
+        }
+      }
+      return newArray;
+    });
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+  }
+
+  return (
+    <main className="container">
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default App;
